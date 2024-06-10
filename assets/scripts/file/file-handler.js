bfa161247fdcb2e4f8d8f78afabdde0defa5d547
mgcvale <miguelcvalealt@gmail.com>
2024-06-10 16:50:22 -0300

    fix: form de upload de imagens

--- assets/scripts/file/file-handler.js
+++ assets/scripts/file/file-handler.js
@@ -1,14 +1,36 @@
-import { getCookie } from "../cookie-manager";
+import { getCookie } from "../cookie-manager.js";
+
 
 console.log("lKAJSLKJSAD");
 document.getElementById("image-upload").addEventListener('submit', function(event) {
     event.preventDefault();
 
-    formData = new FormData(event.target);
+    var formData = new FormData(event.target);
+    formData.append("access_token", getCookie("access_token"));
+    uploadFiles(formData);
 });
 
 function uploadFiles(formData) {
-    const url = "http://localhost:5000/files/test";
+    const url = "http://localhost:5000/files/upload";
     const cookie = getCookie("access_token");
+
+    fetch(url, {
+        method: "POST",
+        body: formData
+    })
+    .then(response => {
+        if(!response.ok) {
+            alert("erro ao enviar mensagens");
+            throw new Error(response.status);
+        }
+        alert("imagens enviadas com sucesso");
+        return response.json;
+    })
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.log("Error: " + error);
+    });
     
 }
